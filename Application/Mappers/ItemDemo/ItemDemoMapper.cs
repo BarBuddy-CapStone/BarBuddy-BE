39337506ebece2838e84bc9d251faf321cee22ed
tien-lai-dev <phamtannhat0@gmail.com>
2024-09-16 09:19:32 +0700

    Config Project

---
+++ Application/Mappers/ItemDemo/ItemDemoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers.ItemDemo
+{
+    public class ItemDemoMapper : Profile
+    {
+        public ItemDemoMapper()
+        {
+
+            #region Item
+            #endregion
+        }
+    }
+}
