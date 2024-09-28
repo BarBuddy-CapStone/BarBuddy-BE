038f83f731879b97a865cc5414eb53df6bb1daa4
LouisKien <louisnamu02@gmail.com>
2024-09-29 01:32:48 +0700

    table type api

---
+++ Application/DTOs/TableTypeDto/TableTypeDtoResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TableTypeDto
+{
+    public class TableTypeDtoResponse
+    {
+        public string? TableTypeId { get; set; }
+        public string? TypeName { get; set; }
+        public string? Description { get; set; }
+        public int MinimumGuest { get; set; }
+        public int MaximumGuest { get; set; }
+        public double MinimumPrice { get; set; }
+    }
+}
