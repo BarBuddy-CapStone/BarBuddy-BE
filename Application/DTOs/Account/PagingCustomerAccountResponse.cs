3af2a2f0df6433556034890d885593a2a4e484e0
tien-lai-dev <phamtannhat0@gmail.com>
2024-11-25 13:46:59 +0700

    Fix paging and fix name add Drirnk

---
+++ Application/DTOs/Account/PagingCustomerAccountResponse.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Account
+{
+    public class PagingCustomerAccountResponse : PagingResponse
+    {
+        public List<CustomerAccountResponse> CustomerAccounts { get; set; }
+    }
+}
