9930e5162e524e14374c960d7353c8c7efb11430
tien-lai-dev <phamtannhat0@gmail.com>
2024-12-03 19:25:56 +0700

    Add Noti Event Job For FE and Mobile

--- Infrastructure/DependencyInjection/DIInfrastructure.cs
+++ Infrastructure/DependencyInjection/DIInfrastructure.cs
@@ -217,17 +217,26 @@ namespace Infrastructure.DependencyInjection
                 AddJobWithTrigger<NotificationJob>(options, nameof(NotificationJob));
                 AddJobWithTrigger<EventJob>(options, nameof(EventJob));
                 AddJobWithTrigger<TokenJob>(options, nameof(TokenJob));
+                AddJobWithTriggerTime<NotificationEventJob>(options, nameof(NotificationEventJob));
             });
 
             services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
         }
-
+        // Start moi phut
         public static void AddJobWithTrigger<TJob>(IServiceCollectionQuartzConfigurator options, string jobName) where TJob : IJob
         {
             var jobKey = JobKey.Create(jobName);
             options.AddJob<TJob>(joinBuilder => joinBuilder.WithIdentity(jobKey))
                    .AddTrigger(trigger => trigger.ForJob(jobKey)
                    .WithCronSchedule("0 * * ? * *"));
+        }
+        // Start luc 16am
+        public static void AddJobWithTriggerTime<TJob>(IServiceCollectionQuartzConfigurator options, string jobName) where TJob : IJob
+        {
+            var jobKey = JobKey.Create(jobName);
+            options.AddJob<TJob>(joinBuilder => joinBuilder.WithIdentity(jobKey))
+                   .AddTrigger(trigger => trigger.ForJob(jobKey)
+                   .WithCronSchedule("0 25 19 * * ?"));
         }
     }
 }
