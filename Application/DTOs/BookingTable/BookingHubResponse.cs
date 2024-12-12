c300f138dd9a2488e8314c1bcd852eb50c751580
LouisKien <louisnamu02@gmail.com>
2024-12-13 02:58:03 +0700

    fix signal r bug

--- Application/DTOs/BookingTable/BookingHubResponse.cs
+++ Application/DTOs/BookingTable/BookingHubResponse.cs
@@ -11,6 +11,7 @@ namespace Application.DTOs.BookingTable
         public Guid AccountId { get; set; }
         public Guid BarId { get; set; }
         public Guid TableId { get; set; }
+        public string TableName { get; set; }
         public DateTimeOffset Date {  get; set; }
         public TimeSpan Time { get; set; }
         public DateTimeOffset HoldExpiry { get; set; }
